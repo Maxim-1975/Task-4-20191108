23da95faa112956589e2fabeeea43a808b245592
Maxim-1975 <mkolpakov@gmail.com>
2019-11-09 13:22:53 +0300

    Forth task

---
+++ Second-Solution-Attempt-2/Second-Solution-Attempt-2/ContactCreationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests:TestBase
+    {
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToAccountPage();
+            InitContactData icd = new InitContactData();
+            icd.Firstname = "Maxim";
+            icd.Middlename = "Vladimirovitch";
+            icd.Lastename = "Kolpakov";
+            InitContactCreation(icd);
+            SubmitContactCreation();
+            ReturnHomePage();
+            Logout();
+        }
+    }
+}
---
+++ Second-Solution-Attempt-2/Second-Solution-Attempt-2/GroupCreationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests:TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("test");
+            group.Header = "test middlename";
+            group.Footer = "test lastname";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            GoToGroupsPage();
+            Logout();
+            OpenHomePage();
+        }
+    }
+}
